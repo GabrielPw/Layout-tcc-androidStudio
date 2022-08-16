ee58b857f6a07de2f172ca12f9ba860345c89a5b
GabrielPw <gxaavier352@gmail.com>
2022-08-16 12:25:29 -0300

    Criação nova Activity.

--- app/src/main/java/com/example/layouttcc/MainActivity.java
+++ app/src/main/java/com/example/layouttcc/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.layouttcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.layouttcc.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.inicio_main);
+    }
+
+    public void servicosAct(View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
     }
 }
